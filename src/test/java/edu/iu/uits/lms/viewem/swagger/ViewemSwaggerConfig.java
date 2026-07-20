fe99a637d1f6d2c5ec41d42713fe6557fcc5f3a4
Damian Sobieralski <dsobiera@iu.edu>
2026-07-20 17:57:12 -0400

    LMSA-9892 - updated license

--- src/test/java/edu/iu/uits/lms/viewem/swagger/ViewemSwaggerConfig.java
+++ src/test/java/edu/iu/uits/lms/viewem/swagger/ViewemSwaggerConfig.java
@@ -4,7 +4,7 @@ package edu.iu.uits.lms.viewem.swagger;
  * #%L
  * lms-canvas-viewem
  * %%
- * Copyright (C) 2015 - 2024 Indiana University
+ * Copyright (C) 2015 - 2026 Indiana University
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
